51c1f9d76bde1c232948a57f21329c59dfad8d4b
Soeking <great.from.g@gmail.com>
2020-04-01 00:44:39 +0900

    mem

---
+++ src/modules/protect/memcpy.s
@@ -0,0 +1,24 @@
+memcpy:
+
+    push    ebp
+    mov     ebp, esp
+
+    push    ecx
+    push    esi
+    push    edi
+
+    cld
+    mov     edi, [ebp + 4]
+    mov     esi, [ebp + 6]
+    mov     ecx, [ebp + 8]
+
+    rep movsb
+
+    pop     edi
+    pop     esi
+    pop     ecx
+
+    mov     esp, ebp
+    pop     ebp
+
+    ret
