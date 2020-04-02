9729418b8c993fb4973a60815bd0aa5b2ebe8256
Soeking <great.from.g@gmail.com>
2020-04-02 22:58:59 +0900

    reboot

---
+++ src/modules/real/reboot.s
@@ -0,0 +1,18 @@
+reboot:
+
+        cdecl   puts, .s0
+
+.10L:
+
+        mov     ah, 0x10
+        int     0x16
+
+        cmp     al, ' '
+        jne     .10L
+
+        cdecl   puts, .s1
+
+        int     0x19
+
+.s0     db  0x0A, 0x0D, "Push SPACE Key to reboot...", 0
+.s1     db  0x0A, 0x0D, 0x0A, 0x0D, 0
