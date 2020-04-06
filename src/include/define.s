83476fca8c510213b4dac2d13af2fb3f99c07b7c
Soeking <great.from.g@gmail.com>
2020-04-06 23:45:16 +0900

    kernel

--- src/include/define.s
+++ src/include/define.s
@@ -3,5 +3,11 @@
         BOOT_SIZE           equ     (1024 * 8)
         SECT_SIZE           equ     (512)
         BOOT_SECT           equ     (BOOT_SIZE / SECT_SIZE)
+        BOOT_END            equ     (BOOT_LOAD + BOOT_SIZE)
 
         E820_RECORD_SIZE    equ     20
+
+        KERNEL_LOAD         equ     0x0010_1000
+
+        KERNEL_SIZE         equ     (1024 * 8)
+        KERNEL_SECT         equ     (KERNEL_SIZE / SECT_SIZE)
