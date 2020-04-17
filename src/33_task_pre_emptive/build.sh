b9a828c5ad8395183f3933d55a1cf956650c71e0
Soeking <great.from.g@gmail.com>
2020-04-17 16:03:14 +0900

    preem

---
+++ src/33_task_pre_emptive/build.sh
@@ -0,0 +1,14 @@
+cd $(
+    cd $(dirname $0)
+    pwd
+)
+
+file="kernel.s"
+
+if [ -e $file ]; then
+    nasm boot.s -o boot.bin -l boot.lst
+    nasm kernel.s -o kernel.bin -l kernel.lst
+    cat boot.bin kernel.bin > boot.img
+else
+    nasm boot.s -o boot.img -l boot.lst
+fi
