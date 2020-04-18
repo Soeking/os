1bf78d9a743aa8ce6b5641c147144037964d9b07
Soeking <great.from.g@gmail.com>
2020-04-19 01:46:27 +0900

    draw rose

--- src/include/macro.s
+++ src/include/macro.s
@@ -74,17 +74,36 @@
 %endmacro
 
 struc drive
-        .no     resw    1
-        .cyln   resw    1
-        .head   resw    1
-        .sect   resw    1
+        .no         resw    1
+        .cyln       resw    1
+        .head       resw    1
+        .sect       resw    1
 endstruc
 
 %define		RING_ITEM_SIZE		(1 << 4)
 %define		RING_INDEX_MASK		(RING_ITEM_SIZE - 1)
 
 struc ring_buff
-        .rp     resd    1
-        .wp     resd    1
-        .item   resb    RING_ITEM_SIZE
+        .rp         resd    1
+        .wp         resd    1
+        .item       resb    RING_ITEM_SIZE
+endstruc
+
+struc rose
+        .x0         resd    1
+        .y0         resd    1
+        .x1         resd    1
+        .y1         resd    1
+
+        .n          resd    1
+        .d          resd    1
+
+        .color_x    resd    1
+        .color_y    resd    1
+        .color_z    resd    1
+        .color_s    resd    1
+        .color_f    resd    1
+        .color_b    resd    1
+
+        .title      resb    16
 endstruc
